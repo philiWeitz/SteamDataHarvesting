0efd9b8930587b136758a53493ae69cb5f7dd00e
silvia <tampeta70@gmail.com>
2015-04-19 15:19:03 +0300

    Added functionalities to add and remove apps from watchlist

--- AngularJS/src/main/app/js/controller/templateCtrl.js
+++ AngularJS/src/main/app/js/controller/templateCtrl.js
@@ -7,12 +7,13 @@ app.constant('Config', {
 app.controller('TemplateCtrl', function ($scope, $http, Config) {
 
     $scope.searchText = '';
+    $scope.watchlist = [];
 
-	$scope.games = [
+	/*$scope.games = [
         {appId: '1', name: 'Game 1', getsUpdated: true},
         {appId: '2', name: 'Game 2', getsUpdated: false},
         {appId: '3', name: 'Game 3', getsUpdated: false}
-    ];
+    ];*/
 
     $scope.getGames = function(){
 
@@ -27,5 +28,31 @@ app.controller('TemplateCtrl', function ($scope, $http, Config) {
                 console.log('error');
             });
     };
+
+    var setWatchlist = function(){
+
+        console.log('posting');
+        console.log($scope.watchlist);
+
+        $http.post('/setWatchList', {watchlist: $scope.watchlist}).
+            success(function(data, status, headers, config) {
+                console.log('post success');
+            }).
+            error(function(data, status, headers, config) {
+                console.log('post error');
+            });
+    };
+
+    $scope.addToWatchlist = function(appId){
+        $scope.watchlist.push(appId);
+        setWatchlist();
+    };
+
+    $scope.removeFromWatchlist = function(appId){
+        $scope.watchlist = _.without($scope.watchlist, appId);
+        setWatchlist();
+    };
+
+    $scope.getGames();
 
 });
